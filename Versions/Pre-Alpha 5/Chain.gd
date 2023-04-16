a7f73f359c26f57eed2319de3d57b5b1842d6519
Anthony Prancl <aprancl@mocs.flsouthern.edu>
2023-04-15 21:50:57 -0400

    working metal surphices

--- Versions/Pre-Alpha 5/Chain.gd
+++ Versions/Pre-Alpha 5/Chain.gd
@@ -10,6 +10,7 @@ var hook_speed = 400
 var hook_position := Vector2(0,0)
 var direction := Vector2(0,0)
 var player_position = Vector2(0,0)
+var collisoin_data;
 
 var is_shot = false
 
@@ -20,6 +21,7 @@ var movement_speed = 3
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	var thing = 10;
 	pass # Replace with function body.
 
 func _process(delta):
@@ -39,12 +41,20 @@ func _process(delta):
 	
 func _physics_process(delta):
 	
+	if $Tip != null:
+		velocity += Vector2(0, gravity)
+		collisoin_data = $Tip.move_and_collide(velocity.normalized() * delta * hook_speed)
 	
-	velocity += Vector2(0, gravity)
-	var collisoin_data = $Tip.move_and_collide(velocity.normalized() * delta * hook_speed)
-	
-
-	if collisoin_data != null:
+	# in the unique case that we hooked something and it is metal
+	if collisoin_data != null and collisoin_data.get_collider() is StaticBody2D:
+			print(collisoin_data.get_collider())
+			velocity = velocity.bounce(collisoin_data.normal)
+			velocity.x *= 0.9
+			velocity.y *= 0.9
+	# or if we catch something more generically
+	elif collisoin_data != null:
+		
+		# stop the hook from moveing
 		hook_speed = 0;
 		gravity = 0;
 		get_tree().get_root().get_node("Main/Player").is_hooked = true;
--- Versions/Pre-Alpha 5/MetalSurface.gd
+++ Versions/Pre-Alpha 5/MetalSurface.gd
@@ -4,6 +4,7 @@ extends Node2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+var i = 1
 
 
 # Called when the node enters the scene tree for the first time.
@@ -17,10 +18,10 @@ func _ready():
 
 
 func _on_Area2D_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
-	#change the velocity of the object, so that it bounces off of the surface. 
-	#var body_shape_node = body.shape_owner_get_owner(body_shape_owner)
-	#print(body_shape_node)
 	
-	#body.velocity.x += body.velocity.x * sign(body.velocity.x)
-	# I think that I might need to cast the boject so that I can access the data member 
+	if i == 1:
+		i += 1
+		return
+	elif body.name == "Tip":
+		pass
 	pass # Replace with function body.
